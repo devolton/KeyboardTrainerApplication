7cee2b7cdbab5514eb6c86aa9a4da4706dc1d6f0
Voloshko Anton <voloshko.03@gmail.com>
2024-04-05 00:35:00 +0300

    Adding three pages(TypingTestPage, TypingTutorPage, LearnPage) and setting up switch beetween them

---
+++ AppPages/Pages/TypingTestPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourseProjectKeyboardApplication.AppPages.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TypingTestPage.xaml
+    /// </summary>
+    public partial class TypingTestPage : Page
+    {
+        public TypingTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,37 @@ namespace CourseProjectKeyboardApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Uri _learnPageUri;
+        private Uri _typingTutorUri;
+        private Uri _typingTestUri;
         public MainWindow()
         {
             InitializeComponent();
+            _typingTutorUri = new Uri("/AppPages/Pages/TypingTutorPage.xaml", UriKind.Relative);
+            _typingTestUri = new Uri("/AppPages/Pages/TypingTestPage.xaml", UriKind.Relative);
+            _learnPageUri = new Uri("/AppPages/Pages/LearnPage.xaml", UriKind.Relative);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+
+        }
+
+        private void TypingTutorMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(_typingTutorUri);
+        }
+
+        private void TypingTestMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(_typingTestUri);
+        }
+
+        private void LearnMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(_learnPageUri);
+
         }
     }
 }
