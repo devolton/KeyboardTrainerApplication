02fc90ec8f92c86aad606a94a24cb2db0104c411
Voloshko Anton <voloshko.03@gmail.com>
2024-04-30 00:38:26 +0300

    Refactoring and changing something deteils in AuthorizationPage

--- View/Windows/AuthorizationWindow.xaml.cs
+++ View/Windows/AuthorizationWindow.xaml.cs
@@ -23,9 +23,11 @@ namespace CourseProjectKeyboardApplication.View.Windows
     {
         private UserLoginPage _userLoginPage;
         private UserSingInPage _userSingInPage;
+        private bool _isLoginPageActive;
         public AuthorizationWindow()
         {
             InitializeComponent();
+            _isLoginPageActive = true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -42,15 +44,55 @@ namespace CourseProjectKeyboardApplication.View.Windows
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
+        private void OnLoginNavigatorButtonClick(object sender, RoutedEventArgs e)
+        {            
             AuthorizationFrame.Content = _userLoginPage;
+            _isLoginPageActive = !_isLoginPageActive;
+            ChangeButtonsStyle();
 
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void OnSingUpNavigatorButtonClick(object sender, RoutedEventArgs e)
         {
             AuthorizationFrame.Content = _userSingInPage;
+            _isLoginPageActive = !_isLoginPageActive;
+            ChangeButtonsStyle();
+        }
+        private void ChangeButtonsStyle()
+        {
+            SolidColorBrush deepVioletBrush = (SolidColorBrush)Application.Current.Resources["CustomDeepBlueViolet"];
+            SolidColorBrush coalBrush = (SolidColorBrush)Application.Current.Resources["CustomCoal"];
+            SolidColorBrush whiteBrush = Brushes.White;
+            ImageSource whiteLoginIcon = (ImageSource)Application.Current.Resources["LoginUserWhiteIcon"];
+            ImageSource blueLoginIcon = (ImageSource)Application.Current.Resources["LoginUserBlueIcon"];
+            ImageSource whiteSingUpIcon = (ImageSource)Application.Current.Resources["SingInUserWhiteIcon"];
+            ImageSource blueSingUpIcon = (ImageSource)Application.Current.Resources["SingInUserBlueIcon"];
+          
+            if (_isLoginPageActive)
+            {
+                LoginNavigatorButton.Foreground = whiteBrush;
+                LoginNavigatorButton.Background = deepVioletBrush;
+                LoginNavigatorIconImage.Source= whiteLoginIcon;
+                LoginNavigatorButtonTextBlock.TextDecorations = TextDecorations.Underline;
+                SingUpNavigatorButton.Foreground = coalBrush;
+                SingUpNavigatorButton.Background = null;
+                SingUpNavigatorIconImage.Source= blueSingUpIcon;
+                SingUpNavigatorButtonTextBlock.TextDecorations = null;
+            }
+            else
+            {
+                SingUpNavigatorButton.Foreground = whiteBrush;
+                SingUpNavigatorButton.Background = deepVioletBrush;
+                SingUpNavigatorButtonTextBlock.TextDecorations = TextDecorations.Underline;
+                SingUpNavigatorIconImage.Source = whiteSingUpIcon;
+                LoginNavigatorButton.Foreground = coalBrush;
+                LoginNavigatorIconImage.Source = blueLoginIcon;
+                LoginNavigatorButton.Background = null;
+                LoginNavigatorButtonTextBlock.TextDecorations = null;
+
+
+            }
+
         }
     }
 }
